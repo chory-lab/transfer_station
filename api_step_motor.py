31ba163eb8923b591d1a6239d732b69813dce1dd
Veniamin Pyatski <69654071+BenyaPyat@users.noreply.github.com>
2024-05-09 15:29:46 -0400

    Update api_step_motor.py

--- api_step_motor.py
+++ api_step_motor.py
@@ -1,15 +1,22 @@
 #Author: Benjamin Pyatski
 #Company: Chory Lab
 
+#to add flask respository 
+#export FLASK_APP=name.py
+
+#to run code
+#flask --app api_step_motor run
+
+import os
 from flask import Flask, render_template, request, jsonify
 import RPi.GPIO as GPIO
 import time
 from flask_caching import Cache
 import redis
-import A4988
-    
+import A4988    
+
 print(__name__)
-app = Flask(__name__, template_folder = '/home/git/transfer_station/templates')
+app = Flask(__name__, template_folder = '/home/chorylab/transfer_station/templates')
 app.config['CACHE_TYPE'] = 'RedisCache' 
 cache = Cache(app)
 
@@ -20,7 +27,7 @@ string_to_bool = {'True':True, 'False': False, None: None}
 # RPi and Motor Pre-allocations
 ################################
 
-GPIO.setmode(GPIO.BCM)
+#GPIO.setmode(GPIO.BOARD)
 
 #define GPIO pins
 direction= 22 # Direction (DIR) GPIO Pin
@@ -28,7 +35,8 @@ step = 23 # Step GPIO Pin
 EN_pin = 24# enable pin (LOW to enable)
 
 # Declare a instance of class pass GPIO pins numbers and the motor type
-my_motor = A4988.A4988Nema(direction, step, (11,9,10), "A4988", cache)
+my_motor = A4988.A4988Nema(direction, step, (11,9,10), "A4988", cache)    
+
 GPIO.setup(EN_pin,GPIO.OUT) # set enable pin as output
 
 cache.set('motor_stop', 'False')
@@ -44,34 +52,36 @@ def index():
         if request.form.get('stop_motor') == 'Stop Motor':
             cache.set('motor_stop', 'True')
             return render_template('buttons.html')
-        
+            
         #dropbox
-        direction = string_to_bool[request.form['direction']]
-        
+        try:
+            direction = string_to_bool[request.form['direction']]
+        except:
+            direction = True
+    
         #hit boxes
         try:
             step_num = int(request.form['step_num'])         
         except:
-            step_num = 280000
-            
+            step_num = 26000
         try:
             step_delay = float(request.form['step_delay'])
         except:
-            step_delay = 0.000000005
-        
-        #button
-        if request.form.get('run_motor') == 'Run Motor':
-            string = move_test(direction, step_num, step_delay)
-            return render_template('buttons.html')
+            step_delay = 0.00000000000005
         
         #go left till end button
-        if request.form.get('go_left') == 'Go Left':
+        if request.form.get('go_left') == 'Go Left' or request.get_data() == b'go_left':
             string = move_test(False, step_num, step_delay)
             return render_template('buttons.html')
         
         #go right till end button
-        if request.form.get('go_right') == "Go Right":
+        if request.form.get('go_right') == "Go Right" or request.get_data() == b'go_right': 
             string = move_test(True, step_num, step_delay)
+            return render_template('buttons.html')
+        
+        #button
+        if request.form.get('run_motor') == 'Run Motor':
+            string = move_test(direction, step_num, step_delay)
             return render_template('buttons.html')
         
     elif request.method == 'GET':        
@@ -103,7 +113,6 @@ def move_test(direction, step_num, step_delay):
                         step_delay, # step delay [sec]
                         True, # True = print verbose output 
                         .05) # initial delay [sec]
-                        
         GPIO.output(EN_pin,GPIO.HIGH)
         
         cache.set('motor_moving', 'False')
@@ -111,4 +120,5 @@ def move_test(direction, step_num, step_delay):
         return "Motor Running"
     
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
